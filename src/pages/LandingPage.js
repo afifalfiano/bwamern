928d17dab3387a1f8c715d2fd2e1104f4d52db66
Afif Alfiano <afif.alfiano@uii.ac.id>
2022-04-28 10:05:28 +0700

    craete header component and put to landing page

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,10 @@
+import Header from 'parts/Header'
+import React from 'react'
+
+export default function LandingPage(props) {
+  return (
+    <React.Fragment>
+    <Header {...props} />
+    </React.Fragment>
+  )
+}
